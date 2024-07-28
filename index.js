198a0f250e7f31a4f25949e204c58cdba6bdffa9
Titus kipchirchir <vantiger817@tgmail.com>
2024-07-29 01:27:45 +0300

    complete_lap

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// Saturday Fun
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Monday Work
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// Wrap Adjective
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
